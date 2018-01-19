9697b749cfb54ca3b603e9f6dd8777638179ae4f
aquaktus <aquaktus@yahoo.co.uk>
2018-01-19 23:46:26 +0000

    haven't committed in a while, and start of the full javaScript version of the code

---
+++ pureJS/serialEcho.js
@@ -0,0 +1,18 @@
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyACM0');
+ 
+port.write('main screen turn on', function(err) {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+  console.log('message written');
+});
+ 
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+  console.log('Error: ', err.message);
+});
+
+port.on('data', function (data) {
+  console.log('Data:', data.toString('utf8'));
+});
